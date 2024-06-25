18e1d2aa3df20a8009d56ddd1f69c114ee3efbaf
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-06-25 01:10:50 -0400

    Nova Função e Estilização
    Foi incluido um cronômetro  e estilização da página

--- jogoJavascript/app.js
+++ jogoJavascript/app.js
@@ -1,6 +1,18 @@
  const button = document.querySelector("#button")
+ let minutos = document.getElementById('minutos')
+ let segundos = document.getElementById('segundos')
+ let min = '00'
+ let seg = '00'
+ let tempoCorrido = false
+ let resposta = null
+
+
 
  button.addEventListener('click', function() {
+      tempoCorrido = true
+      min ='00'
+      seg = '00'
+      resposta = null
         fetch('question_response.json')
             .then(response => {
                 if (!response.ok) {
@@ -19,10 +31,57 @@
 
      function tratarJson(file){
         let random = Math.floor(Math.random()*file.test.length)
-        console.log(file)
+            resposta = file.test[random].response
             return file.test[random].question
          
      }
+
+     function duracaoTempo(valor){
+        min = valor
+        if(min < 10){
+          min = '0'+valor
+          console.log(min)
+        }
+        document.getElementById('')
+      }
+
+     function cronometro(valor){
+    if(seg == 0){
+        seg = 60
+    }
+    if(tempoCorrido){
+      seg -= valor
+      minutos.innerHTML = min
+      segundos.innerHTML = seg
+      if(seg < 10){
+        segundos.innerHTML = '0'+seg 
+      }
+      if(seg == 0){
+        min --
+        minutos.innerHTML = '0'+min
+        if(min > 0){
+            seg = 60
+        }
+        if(min < 0 ){
+            minutos.innerHTML = '00' 
+        }
+ 
+    
+      }
+      if(min <= 0 && seg <= 0){
+        tempoCorrido = false
+        mostrarResposta()
+      }
+    }
+
+     }
+
+     function mostrarResposta(){
+        console.log(resposta)
+     }
+
+     setInterval(cronometro,1000,1)
+
 
 
 
