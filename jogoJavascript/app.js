c80179cc8ea4df9b7435fe2d27dd55629586f872
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-06-26 12:28:47 -0400

    Novas funcionalidades
    melhorado a lógica do tempo, incluido novas funcionalidade.

--- jogoJavascript/app.js
+++ jogoJavascript/app.js
@@ -1,92 +1,108 @@
- const button = document.querySelector("#button")
- let minutos = document.getElementById('minutos')
- let segundos = document.getElementById('segundos')
- let min = '00'
- let seg = '00'
- let tempoCorrido = false
- let resposta = null
+const button = document.querySelector("#button")
+let minutos = document.getElementById('minutos')
+let segundos = document.getElementById('segundos')
+let min = '00'
+let seg = '00'
+let tempoCorrido = false
+let resposta = null
 
 
 
- button.addEventListener('click', function() {
-      tempoCorrido = true
-      min ='00'
-      seg = '00'
-      resposta = null
-        fetch('question_response.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro ao carregar o arquivo JSON: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                document.getElementById('output').textContent = tratarJson(data)
-             
-            })
-            .catch(error => {
-                document.getElementById('output').textContent = 'Erro: ' + error.message;
-            });
+
+button.addEventListener('click', function () {
+  tempoCorrido = true
+  seg = '00'
+  resposta = null
+  document.getElementById('resposta').className = ''
+  if(document.getElementById('respostaJson')){
+    document.getElementById('tempo').removeChild(document.getElementById('respostaJson'))
+  }
+  duracaoTempo(document.getElementById('duracaoTempo').value)
+  fetch('question_response.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao carregar o arquivo JSON: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.getElementById('output').textContent = tratarJson(data)
+    })
+    .catch(error => {
+      document.getElementById('output').textContent = 'Erro: ' + error.message;
     });
+});
 
-     function tratarJson(file){
-        let random = Math.floor(Math.random()*file.test.length)
-            resposta = file.test[random].response
-            return file.test[random].question
-         
-     }
+function tratarJson(file) {
+  let random = Math.floor(Math.random() * file.test.length)
+  resposta = file.test[random].response
+  return file.test[random].question
 
-     function duracaoTempo(valor){
-        min = valor
-        if(min < 10){
-          min = '0'+valor
-          console.log(min)
-        }
-        document.getElementById('')
-      }
+}
 
-     function cronometro(valor){
-    if(seg == 0){
-        seg = 60
-    }
-    if(tempoCorrido){
-      seg -= valor
-      minutos.innerHTML = min
-      segundos.innerHTML = seg
-      if(seg < 10){
-        segundos.innerHTML = '0'+seg 
-      }
-      if(seg == 0){
-        min --
-        minutos.innerHTML = '0'+min
-        if(min > 0){
-            seg = 60
-        }
-        if(min < 0 ){
-            minutos.innerHTML = '00' 
-        }
+function duracaoTempo(valor) {
+  if(valor == '' ){
+    valor = 0
+  }
+  if (valor < 10 ) {
+    min = '0' + valor
+  }else{
+    min = valor
+  }
  
-    
+}
+
+function cronometro(valor) {
+  if (seg == 0 ) {
+    seg = 60
+    min = min == 0? '00' : min
+  }
+  if (tempoCorrido) {
+    seg -= valor
+    minutos.innerHTML = min
+    segundos.innerHTML = seg
+    segundos.innerHTML = (seg < 10) ? '0' + seg : seg
+  if (seg == 0) {
+      min--
+      if(min >= 0){
+        min = (min < 10) ? '0' + min : min
+        minutos.innerHTML = min
       }
-      if(min <= 0 && seg <= 0){
-        tempoCorrido = false
-        mostrarResposta()
+      console.log(min)
+      if (min > 0) {
+        seg = 60
       }
+      else {
+        minutos.innerHTML = '00'
+       }
     }
+  if (min < 0 && seg <= 0) {
+      tempoCorrido = false
+      mostrarResposta()
+    }
+  }
+}
 
-     }
+setInterval(cronometro, 1000, 1)
 
-     function mostrarResposta(){
-        console.log(resposta)
-     }
-
-     setInterval(cronometro,1000,1)
-
-
-
-
+function mostrarResposta() {
+   document.getElementById('resposta').className = 'hide'
+   let p = document.createElement('p')
+   p.id= 'respostaJson'
+   p.innerHTML = resposta
+   document.getElementById('tempo').appendChild(p)
+}
 
 
 
+function settings(){
+  let configuracao = document.getElementById('configuracao')
+  configuracao.classList.toggle('hide')
+}
 
-  
+
+
+
+
+
+
