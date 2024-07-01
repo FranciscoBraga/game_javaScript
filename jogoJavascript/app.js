d4bdb911704de033b2445e7bc6772bb65d16d85b
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-07-01 00:02:41 -0400

    Níveis e Tecnologias
    Temos a opção de selecionar o nível da pergunta e da tecnologia

--- jogoJavascript/app.js
+++ jogoJavascript/app.js
@@ -5,20 +5,28 @@ let min = '00'
 let seg = '00'
 let tempoCorrido = false
 let resposta = null
+let level = null
+let op = null
 
-
-
-
-button.addEventListener('click', function () {
+function inicializa() {
   tempoCorrido = true
   seg = '00'
   resposta = null
   document.getElementById('resposta').className = ''
-  if(document.getElementById('respostaJson')){
+  level = document.querySelector("input[name='level']:checked").id
+  level = (level == null) ? 'easy' : level
+  let sobre = document.getElementById("sobre")
+  op = sobre.options[sobre.selectedIndex].value
+  console.log(op)
+  if (document.getElementById('respostaJson')) {
     document.getElementById('tempo').removeChild(document.getElementById('respostaJson'))
   }
   duracaoTempo(document.getElementById('duracaoTempo').value)
-  fetch('question_response.json')
+}
+
+button.addEventListener('click', function () {
+  inicializa()
+  fetch('question_response_' + level + '_' + op + '.json')
     .then(response => {
       if (!response.ok) {
         throw new Error('Erro ao carregar o arquivo JSON: ' + response.statusText);
@@ -41,30 +49,30 @@ function tratarJson(file) {
 }
 
 function duracaoTempo(valor) {
-  if(valor == '' ){
+  if (valor == '') {
     valor = 0
   }
-  if (valor < 10 ) {
+  if (valor < 10) {
     min = '0' + valor
-  }else{
+  } else {
     min = valor
   }
- 
+
 }
 
 function cronometro(valor) {
-  if (seg == 0 ) {
+  if (seg == 0) {
     seg = 60
-    min = min == 0? '00' : min
+    min = min == 0 ? '00' : min
   }
   if (tempoCorrido) {
     seg -= valor
     minutos.innerHTML = min
     segundos.innerHTML = seg
     segundos.innerHTML = (seg < 10) ? '0' + seg : seg
-  if (seg == 0) {
+    if (seg == 0) {
       min--
-      if(min >= 0){
+      if (min >= 0) {
         min = (min < 10) ? '0' + min : min
         minutos.innerHTML = min
       }
@@ -74,9 +82,9 @@ function cronometro(valor) {
       }
       else {
         minutos.innerHTML = '00'
-       }
+      }
     }
-  if (min < 0 && seg <= 0) {
+    if (min < 0 && seg <= 0) {
       tempoCorrido = false
       mostrarResposta()
     }
@@ -86,16 +94,14 @@ function cronometro(valor) {
 setInterval(cronometro, 1000, 1)
 
 function mostrarResposta() {
-   document.getElementById('resposta').className = 'hide'
-   let p = document.createElement('p')
-   p.id= 'respostaJson'
-   p.innerHTML = resposta
-   document.getElementById('tempo').appendChild(p)
+  document.getElementById('resposta').className = 'hide'
+  let p = document.createElement('p')
+  p.id = 'respostaJson'
+  p.innerHTML = resposta
+  document.getElementById('tempo').appendChild(p)
 }
 
-
-
-function settings(){
+function settings() {
   let configuracao = document.getElementById('configuracao')
   configuracao.classList.toggle('hide')
 }
